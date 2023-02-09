6870790e488734dfabbed128b44d62a01bdda7bd
Jensbjorgo <snej765@gmail.com>
2023-02-09 13:10:33 +0100

    prøver å få til en fungerende search bar

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+import json
+
 
 app = Flask(__name__)
 
+fil=open("ovelser.json")
+ovelser=json.load(fil)
+
+
+def sok_ovelse(okt):
+    resultater = []
+    for ovelse in ovelser:
+        if okt.lower() in ovelse["navn"].lower() or okt.lower() in ovelse["treningstype"].lower():
+            resultater.append(ovelse)
+    return resultater
+
+
+
 @app.route("/")
 def index():
-    navn = "JEns"
-    return render_template("index.html", navn=navn)
+    sok_ovelse("m")
+    okt = request.args.get("m")
+    if okt:
+        resultater = sok_ovelse(okt)
+    else:
+        resultater = []
+    
+    return render_template("index.html",ovelser=ovelser,resultater=resultater,okt=okt)
 
 app.run(debug=True)
